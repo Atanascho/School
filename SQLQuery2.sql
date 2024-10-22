b917b30dc17307ae9cb5af023c931e35140b601a
Atanascho <123800724+Atanascho@users.noreply.github.com>
2024-10-22 16:50:52 +0300

    SQLQuery2.sql

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,7 +1,45 @@
 --Homework
-
---Zad 6
+	
+--ZAD 1
 USE SoftUni
+SELECT TOP 5 EmployeeId, JobTitle, a.AddressId, AddressText
+FROM Employees AS e
+JOIN Addresses AS a ON a.AddressID = e.AddressID
+ORDER BY a.AddressID ASC
+
+--ZAD2
+SELECT TOP 5 EmployeeId, FirstName, Salary, d.Name AS DepartmentName
+FROM Employees AS e
+JOIN Departments AS d ON d.DepartmentID = e.DepartmentID
+WHERE Salary > 15000
+ORDER BY d.DepartmentID ASC
+
+--ZAD3
+SELECT TOP 3 e.EmployeeID, FirstName
+FROM Employees AS e
+LEFT JOIN EmployeesProjects AS ep ON ep.EmployeeID = e.EmployeeID
+WHERE ep.ProjectID IS NULL
+ORDER BY e.EmployeeID ASC
+
+--ZAD 4 DATEPART
+SELECT TOP 5 e.EmployeeID, FirstName, p.Name AS ProjectName
+FROM Employees AS e
+JOIN EmployeesProjects AS ep ON ep.EmployeeID = e.EmployeeID
+JOIN Projects AS p ON p.ProjectID = ep.ProjectID
+WHERE StartDate > DATEPART(YEAR,'2002/08/13') AND EndDate IS NULL
+ORDER BY e.EmployeeID ASC
+
+--ZAD 5
+SELECT TOP 5 e.EmployeeID, FirstName,
+CASE
+	WHEN p.StartDate> DATEPART(YEAR,'2005') THEN 'NULL' 
+	ELSE Name
+	END
+FROM Projects AS p
+LEFT JOIN EmployeesProjects AS ep ON ep.ProjectID = p.ProjectID
+LEFT JOIN Employees AS e ON e.EmployeeID = ep.ProjectID
+WHERE e.EmployeeID = 24
+--Zad 6
 
 SELECT me.EmployeeID, me.FirstName, me.ManagerID, e.FirstName AS ManagerName
 FROM Employees AS e
